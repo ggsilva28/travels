1356ed48347a2868f39666b0edf067107a360baa
Gabriel Gonçalves <gabriel@talentacademy.com.br>
2024-05-29 23:25:58 -0300

    feat: :sparkles: Content dinamic

--- src/components/menu.ts
+++ src/components/menu.ts
@@ -1,17 +1,20 @@
 export const scrollBlur = () => {
-    const el: Element = document.querySelector(".menu-container");
+    let el: Element = document.querySelector(".menu-container");
 
     if (!el) return;
 
 
-    window.addEventListener("scroll", function (event) {
+    window.addEventListener("scroll", function () {
         const scrollDistance: number = window.scrollY || 0;
-
 
         if (scrollDistance > 0) {
             el.classList.add("blurry");
         } else {
             el.classList.remove("blurry");
         }
+    });
+
+    document.addEventListener('astro:after-swap', () => {
+        el = document.querySelector(".menu-container");
     });
 };
