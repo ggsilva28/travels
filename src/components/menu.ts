b5cab68c3c3021efe8462f4315ffef28691a24e3
Gabriel Gonçalves <gabriel@talentacademy.com.br>
2024-05-30 23:13:01 -0300

    feat: :sparkles: Mobile

--- src/components/menu.ts
+++ src/components/menu.ts
@@ -1,20 +1,23 @@
 export const scrollBlur = () => {
-    let el: Element = document.querySelector(".menu-container");
+	let el: Element = document.querySelector(".menu-container");
 
-    if (!el) return;
+	if (!el) return;
 
+	window.addEventListener("scroll", function () {
+		const scrollDistance: number = window.scrollY || 0;
 
-    window.addEventListener("scroll", function () {
-        const scrollDistance: number = window.scrollY || 0;
+		if (scrollDistance > 0) {
+			el.classList.add("blurry");
+		} else {
+			el.classList.remove("blurry");
+		}
+	});
+};
 
-        if (scrollDistance > 0) {
-            el.classList.add("blurry");
-        } else {
-            el.classList.remove("blurry");
-        }
-    });
+export const toggleMobile = () => {
+    const menuContainer: Element = document.querySelector(".menu-container");
+	const menuMobile = document.querySelector(".menu-container-mobile");
 
-    document.addEventListener('astro:after-swap', () => {
-        el = document.querySelector(".menu-container");
-    });
+	menuMobile.classList.toggle("open");
+    menuContainer.classList.toggle("menu-mobile-open");
 };
